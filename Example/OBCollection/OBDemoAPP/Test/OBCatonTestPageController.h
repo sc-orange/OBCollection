30c198926c6cf55ebc42b7b70a3cd4a322508166
K1081282 <k1081282@test.htsc.com.cn>
2021-03-24 10:31:34 +0800

    新建卡顿测试页

---
+++ Example/OBCollection/OBDemoAPP/Test/OBCatonTestPageController.h
@@ -0,0 +1,17 @@
+//
+//  OBCatonTestPageController.h
+//  OBCollection_Example
+//
+//  Created by orange on 2021/3/24.
+//  Copyright © 2021 200887744@qq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OBCatonTestPageController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/OBCollection/OBDemoAPP/Test/OBCatonTestPageController.m
@@ -0,0 +1,32 @@
+//
+//  OBCatonTestPageController.m
+//  OBCollection_Example
+//
+//  Created by orange on 2021/3/24.
+//  Copyright © 2021 200887744@qq.com. All rights reserved.
+//
+
+#import "OBCatonTestPageController.h"
+
+@interface OBCatonTestPageController ()
+
+@end
+
+@implementation OBCatonTestPageController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
