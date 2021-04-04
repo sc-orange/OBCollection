cf278cdd87495684ff90c1edeef0c2be95866131
sc-orange <3210492660@qq.com>
2021-04-04 12:55:44 +0800

    OBWebViewCollect创建

---
+++ OBCollection/Classes/OBPerformance/OBWebViewCollect/OBWebViewCollect.h
@@ -0,0 +1,16 @@
+//
+//  OBWebViewCollect.h
+//  OBCollection
+//
+//  Created by orange on 2021/4/4.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OBWebViewCollect : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OBCollection/Classes/OBPerformance/OBWebViewCollect/OBWebViewCollect.m
@@ -0,0 +1,12 @@
+//
+//  OBWebViewCollect.m
+//  OBCollection
+//
+//  Created by orange on 2021/4/4.
+//
+
+#import "OBWebViewCollect.h"
+
+@implementation OBWebViewCollect
+
+@end
