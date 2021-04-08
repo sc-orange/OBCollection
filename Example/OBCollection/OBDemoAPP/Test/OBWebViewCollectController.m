3327299e4b65ccdfa3758cf94f052affe05a1410
K1081282 <k1081282@test.htsc.com.cn>
2021-04-08 16:47:03 +0800

    js、oc交互

--- Example/OBCollection/OBDemoAPP/Test/OBWebViewCollectController.m
+++ Example/OBCollection/OBDemoAPP/Test/OBWebViewCollectController.m
@@ -7,8 +7,13 @@
 //
 
 #import "OBWebViewCollectController.h"
+#import <WebKit/WebKit.h>
 
-@interface OBWebViewCollectController ()
+#define SCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
+#define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
+
+@interface OBWebViewCollectController () <WKScriptMessageHandler, WKUIDelegate, WKNavigationDelegate>
+@property (strong, nonatomic) WKWebView *webView;
 
 @end
 
@@ -16,7 +21,220 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    [self.view addSubview:self.webView];
+}
+
+- (void)dealloc{
+    //移除注册的js方法
+    [[_webView configuration].userContentController removeScriptMessageHandlerForName:@"jsToOcNoPrams"];
+    [[_webView configuration].userContentController removeScriptMessageHandlerForName:@"jsToOcWithPrams"];
+}
+
+#pragma mark - action
+- (IBAction)backAction:(UIButton *)sender {
+    [self.webView goBack];
+}
+
+- (IBAction)refreshAction:(UIButton *)sender {
+    [self.webView reload];
+}
+
+- (IBAction)ocToJs:(UIButton *)sender {
+    NSString *jsString = [NSString stringWithFormat:@"changeColor('%@')", @"Js颜色参数"];
+    [_webView evaluateJavaScript:jsString completionHandler:^(id _Nullable data, NSError * _Nullable error) {
+        NSLog(@"改变HTML的背景色");
+    }];
+    
+    //改变字体大小 调用原生JS方法
+    NSString *jsFont = [NSString stringWithFormat:@"document.getElementsByTagName('body')[0].style.webkitTextSizeAdjust= '%d%%'", arc4random()%99 + 100];
+    [_webView evaluateJavaScript:jsFont completionHandler:nil];
+}
+
+- (IBAction)toHome:(UIButton *)sender {
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"OBJSPage.html" ofType:nil];
+    NSString *htmlString = [[NSString alloc]initWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+    [self.webView loadHTMLString:htmlString baseURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] bundlePath]]];
+}
+
+#pragma mark - WKScriptMessageHandler
+/*
+ 处理js调用oc方法
+ */
+- (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message {
+    //用message.body获得JS传出的参数体
+    NSDictionary * parameter = message.body;
+    //JS调用OC
+    if([message.name isEqualToString:@"jsToOcNoPrams"]){
+        NSLog(@"name:%@\n body:%@\n frameInfo:%@\n",message.name,message.body,message.frameInfo);
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"js调用到了oc" message:@"不带参数" preferredStyle:UIAlertControllerStyleAlert];
+        [alertController addAction:([UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        }])];
+        [self presentViewController:alertController animated:YES completion:nil];
+    }else if([message.name isEqualToString:@"jsToOcWithPrams"]){
+        NSLog(@"name:%@\n body:%@\n frameInfo:%@\n",message.name,message.body,message.frameInfo);
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"js调用到了oc" message:parameter[@"params"] preferredStyle:UIAlertControllerStyleAlert];
+        [alertController addAction:([UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        }])];
+        [self presentViewController:alertController animated:YES completion:nil];
+    }
+    
+}
+
+#pragma mark - WKNavigationDelegate
+/*
+ WKNavigationDelegate主要处理一些跳转、加载处理操作
+ */
+// 页面开始加载时调用
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
+}
+
+// 页面加载失败时调用
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error {
+}
+
+// 当内容开始返回时调用
+- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation {
+    
+}
+
+// 页面加载完成之后调用
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+}
+
+//提交发生错误时调用
+- (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error {
+}
+
+// 接收到服务器跳转请求即服务重定向时之后调用
+- (void)webView:(WKWebView *)webView didReceiveServerRedirectForProvisionalNavigation:(WKNavigation *)navigation {
+}
+
+// 根据WebView对于即将跳转的HTTP请求头信息和相关信息来决定是否跳转
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+    NSString * urlStr = navigationAction.request.URL.absoluteString;
+    if([urlStr isEqualToString:@"https://www.jianshu.com/"]){
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"截取URL提示是否进入" message:@"你想前往简书？" preferredStyle:UIAlertControllerStyleAlert];
+        [alertController addAction:([UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+            decisionHandler(WKNavigationActionPolicyCancel);
+        }])];
+        [alertController addAction:([UIAlertAction actionWithTitle:@"打开" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            decisionHandler(WKNavigationActionPolicyAllow);
+        }])];
+        [self presentViewController:alertController animated:YES completion:nil];
+    }else{
+        decisionHandler(WKNavigationActionPolicyAllow);
+    }
+}
+
+// 根据客户端受到的服务器响应头以及response相关信息来决定是否可以跳转
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler{
+    NSString * urlStr = navigationResponse.response.URL.absoluteString;
+    NSLog(@"当前跳转地址：%@",urlStr);
+    //允许跳转
+    decisionHandler(WKNavigationResponsePolicyAllow);
+    //不允许跳转
+    //decisionHandler(WKNavigationResponsePolicyCancel);
+}
+
+//需要响应身份验证时调用 同样在block中需要传入用户身份凭证
+- (void)webView:(WKWebView *)webView didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential * _Nullable credential))completionHandler{
+//    //用户身份信息
+//    NSURLCredential * newCred = [[NSURLCredential alloc] initWithUser:@"user123" password:@"123" persistence:NSURLCredentialPersistenceNone];
+//    //为 challenge 的发送方提供 credential
+//    [challenge.sender useCredential:newCred forAuthenticationChallenge:challenge];
+//    completionHandler(NSURLSessionAuthChallengeUseCredential,newCred);
+}
+//进程被终止时调用
+- (void)webViewWebContentProcessDidTerminate:(WKWebView *)webView{
+}
+
+#pragma mark - WKUIDelegate
+/*
+ WKUIDelegate主要处理JS脚本，确认框，警告框等
+ */
+
+/**
+ *  web界面中有弹出警告框时调用
+ *
+ *  @param webView           实现该代理的webview
+ *  @param message           警告框中的内容
+ *  @param completionHandler 警告框消失调用
+ */
+// 确认框：JavaScript调用alert方法后回调的方法
+- (void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(void))completionHandler {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"HTML的弹出框" message:message?:@"" preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:([UIAlertAction actionWithTitle:@"确认" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        completionHandler();
+    }])];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+// 确认框/取消框：JavaScript调用confirm方法后回调的方法 confirm是js中的确定框，需要在block中把用户选择的情况传递进去
+- (void)webView:(WKWebView *)webView runJavaScriptConfirmPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(BOOL))completionHandler{
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"" message:message?:@"" preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:([UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        completionHandler(NO);
+    }])];
+    [alertController addAction:([UIAlertAction actionWithTitle:@"确认" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        completionHandler(YES);
+    }])];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+// 输入框
+//JavaScript调用prompt方法后回调的方法 prompt是js中的输入框 需要在block中把用户输入的信息传入
+- (void)webView:(WKWebView *)webView runJavaScriptTextInputPanelWithPrompt:(NSString *)prompt defaultText:(NSString *)defaultText initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(NSString * _Nullable))completionHandler{
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:prompt message:@"" preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.text = defaultText;
+    }];
+    [alertController addAction:([UIAlertAction actionWithTitle:@"确认" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        completionHandler(alertController.textFields[0].text?:@"");
+    }])];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+#pragma mark - getters and setters
+- (WKWebView *)webView{
+    if (!_webView) {
+        //创建网页配置对象
+        WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
+        //创建设置对象
+        WKPreferences *preference = [[WKPreferences alloc]init];
+        //最小字体大小 当将javaScriptEnabled属性设置为NO时，可以看到明显的效果
+        preference.minimumFontSize = 0;
+        //设置是否支持javaScript 默认是支持的
+        preference.javaScriptEnabled = YES;
+        //在iOS上默认为NO，表示是否允许不经过用户交互由javaScript自动打开窗口
+        preference.javaScriptCanOpenWindowsAutomatically = YES;
+        config.preferences = preference;
+        
+        //这个类主要用来做native与JavaScript的交互管理
+        WKUserContentController * content = [[WKUserContentController alloc] init];
+        //注册一个name为jsToOcNoPrams的js方法 设置处理接收JS方法的对象
+        [content addScriptMessageHandler:self  name:@"jsToOcNoPrams"];
+        [content addScriptMessageHandler:self  name:@"jsToOcWithPrams"];
+        config.userContentController = content;
+        
+        //以下代码适配文本大小
+        NSString *jSString = @"var meta = document.createElement('meta'); meta.setAttribute('name', 'viewport'); meta.setAttribute('content', 'width=device-width'); document.getElementsByTagName('head')[0].appendChild(meta);";
+        //用于进行JavaScript注入
+        WKUserScript *wkUScript = [[WKUserScript alloc] initWithSource:jSString injectionTime:WKUserScriptInjectionTimeAtDocumentEnd forMainFrameOnly:YES];
+        [config.userContentController addUserScript:wkUScript];
+        
+        _webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 150, SCREEN_WIDTH, SCREEN_HEIGHT - 250) configuration:config];
+        // UI代理
+        _webView.UIDelegate = self;
+        // 导航代理
+        _webView.navigationDelegate = self;
+        // 是否允许手势左滑返回上一级, 类似导航控制的左滑返回
+        _webView.allowsBackForwardNavigationGestures = YES;
+        
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"OBJSPage.html" ofType:nil];
+        NSString *htmlString = [[NSString alloc]initWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+        [_webView loadHTMLString:htmlString baseURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] bundlePath]]];
+    }
+    return _webView;
 }
 
 @end
